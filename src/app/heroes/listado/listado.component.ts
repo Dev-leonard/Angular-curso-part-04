0508e646de8c95bd3f5c3a72f0a310671b7f9497
Leon Matias <rleonm@proveedor.intercorp.com.pe>
2022-01-27 10:56:14 -0500

    Directiva *ngIf

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -10,6 +10,6 @@ export class ListadoComponent {
   heroeBorrado: string = '';
 
   borrarHeroe() {
-    this.heroeBorrado = this.heroes.shift()||'';
+    this.heroeBorrado = this.heroes.shift() || '';
   }
 }
